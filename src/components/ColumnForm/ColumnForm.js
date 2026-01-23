95563d14d61a0b375047a0fc0487aec13a3fd9e3
Wojciech Johaniuk <wojciech.johaniuk@gmail.com>
2026-01-23 15:25:11 +0100

    add cart's

---
+++ src/components/ColumnForm/ColumnForm.js
@@ -0,0 +1,27 @@
+import styles from './ColumnForm.module.scss';
+import { useState } from 'react';
+import Button from './../Button/Button';
+import TextInput from './../TextInput/TextInput';
+
+const ColumnForm = (props) => {
+	const [icon, setIcon] = useState('');
+	const [title, setTitle] = useState('');
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		props.action({ title: title, icon: icon });
+		setTitle('');
+		setIcon('');
+	};
+
+	return (
+		<form className={styles.columnForm} onSubmit={handleSubmit}>
+			<label>Title:</label>
+			<TextInput value={title} onChange={(e) => setTitle(e.target.value)} />
+			<label>Icon:</label>
+			<TextInput value={icon} onChange={(e) => setIcon(e.target.value)} />
+			<Button>Add column</Button>
+		</form>
+	);
+};
+
+export default ColumnForm;
